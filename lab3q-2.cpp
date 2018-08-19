5211cd4dac59fb997e7b6e84c7b41fd9ce5d333c
Lipsagamango <42207107+Lipsagamango@users.noreply.github.com>
2018-08-19 17:55:55 +0530

    Add files via upload

---
+++ lab3q-2.cpp
@@ -0,0 +1,18 @@
+// first include the library
+#include <iostream>
+using namespace std;
+// main function
+int main(){
+// declaring variables
+int a, b;
+// assigning values
+a = 9;
+b = 3;
+// displaying the values
+cout<< a+b <<endl;
+cout<< a-b <<endl;
+cout<< a*b <<endl;
+cout<< a/b <<endl;
+cout<< a%b <<endl;
+}
+
